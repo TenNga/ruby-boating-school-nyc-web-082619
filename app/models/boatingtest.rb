c7e4edd149eaea6184e324d2280e30d9fe38c111
Kevin Karma <kevinkarma55@gmail.com>
2019-09-01 18:34:21 +0000

    Automatically backed up by Learn

--- app/models/boatingtest.rb
+++ app/models/boatingtest.rb
@@ -1,3 +1,17 @@
 class BoatingTest
-
+  
+  attr_accessor :student, :boatingTestName, :boatingTestSatus
+  @@all = []
+  
+  def initialize(student,boatingTestName,boatingTestSatus)
+    @student = student
+    @boatingTestName = boatingTestName
+    @boatingTestSatus = boatingTestSatus
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
 end
