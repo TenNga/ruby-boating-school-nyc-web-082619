24ee58651a6a13b2194c2c5192a0fc881535e0a8
Kevin Karma <kevinkarma55@gmail.com>
2019-09-01 18:57:04 +0000

    Automatically backed up by Learn

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -8,6 +8,11 @@ class Instructor
   
   def pass_student(student,boatingTestName)
     findStudent = BoatingTest.all.find { |test| test.boatingTestName == boatingTestName and test.student == student }
+    if findStudent
+      findStudent
+    else
+      
+    end
   end
   
   def self.all 
