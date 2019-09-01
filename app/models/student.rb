b6727dc3a6c733367bccfc8b2b78c7e267ed7d24
Kevin Karma <kevinkarma55@gmail.com>
2019-09-01 18:37:42 +0000

    Automatically backed up by Learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -4,8 +4,12 @@ class Student
     @first_name = first_name
   end
   
+  def add_boating_test(boatingTestname,boatingTestStatus,instructor)
+    BoatingTest.new()
+  end
+  
   def self.all 
-      Boat
+      BoatingTest.all.map { |test| test.student }
   end 
   
   
