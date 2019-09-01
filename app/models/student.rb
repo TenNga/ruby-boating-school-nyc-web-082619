40ad6fe049e9f9224cf960cb2fba7916d8d39d8e
Kevin Karma <kevinkarma55@gmail.com>
2019-09-01 18:41:10 +0000

    Automatically backed up by Learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,16 +1,22 @@
 class Student
+  
+  attr_accessor :first_name
 
   def initialize(first_name)
     @first_name = first_name
   end
   
   def add_boating_test(boatingTestname,boatingTestStatus,instructor)
-    BoatingTest.new()
+    BoatingTest.new(self,boatingTestname,boatingTestStatus,instructor)
   end
   
   def self.all 
       BoatingTest.all.map { |test| test.student }
   end 
+  
+  def self.find_student(first_name)
+    self.all.find {|student| student.first_name == first_name }
+  end
   
   
 
